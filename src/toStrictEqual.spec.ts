3cc8906803bdf2efc24b9037f17f368b38b94d55
Bong <borobong230@gmail.com>
2024-10-29 02:48:04 +0900

    :hammer: [Update] toStrictEqual 테스트 케이스 추가 #main

--- src/toStrictEqual.spec.ts
+++ src/toStrictEqual.spec.ts
@@ -1,6 +1,11 @@
 import { obj } from "./toStrictEqual";
 
-test("adds 1 + 2 to equal 3", () => {
-  // expect(obj()).toBe({ a: "hello" }); // 이 테스트는 실패하게 됨.
+test("객체 끼리의 비교는 toStrictEqual 로 해야 함.", () => {
+  expect(obj()).not.toBe({ a: "hello" }); // 이 테스트는 실패하게 됨.
   expect(obj()).toStrictEqual({ a: "hello" }); // 이 테스트는 성공하게 됨. - 객체 끼리의 비교는 toStrictEqual 로 해야 함.
+});
+
+test("배열 끼리의 비교도 toStrictEqual 로 해야 함.", () => {
+  expect([1, 2, 3]).toStrictEqual([1, 2, 3]);
+  expect([1, 2, 3]).not.toBe([1, 2, 3]);
 });
